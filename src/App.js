3a7f52c6af0ef6f6261a4de7e56babcb6f5eaa7c
Yaaqoob Choulli <yaaqoobchoulli@Yaaqoobs-MBP.ed.shawcable.net>
2023-11-15 10:05:47 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Navbar' ;
+import HomePage from './Pages/HomePage';
+import ShopPage from './Pages/ShopPage';
+import ContactPage from './Pages/ContactPage';
+import CartPage from './Pages/CartPage';
+//import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar /> {/* Calls on Navbar function */}
+        
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
